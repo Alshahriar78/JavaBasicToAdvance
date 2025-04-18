1923187f20d9df545dcb0cde70b5cc2714e1306b
Alshahriar78 <alshahriar78@gmail.com>
2025-04-18 23:56:33 +0600

    Loop statments

---
+++ com/bazlur/java/Loops.java
@@ -0,0 +1,13 @@
+package com.bazlur.java;
+
+public class Loops {
+
+    public static void main(String[] args) {
+        double result = 5;
+        // for loop
+        for (int i = 1; i < 10; i++) {
+            // namota
+            System.out.println(result + "x"+ i + " = " +(result *i));
+        }
+    }
+}
